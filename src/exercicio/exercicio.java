1b2c30877e7744cc6bac0ec4d125ebb9577864e5
CelsoTNeto <cteixeiraneto@hotmail.com>
2018-09-10 22:25:58 -0300

    exercicios java

---
+++ src/exercicio/exercicio.java
@@ -0,0 +1,20 @@
+package exercicio;
+
+public class exercicio {
+
+	public static void main(String[] args) {
+		int r = 12;
+		double area  = 3.14 * (r * r);
+		int resto = 99 % 4;
+		int x = 10;
+	    double resultado = x * 0.50 ;
+	    double n = 3;
+	    double mult = n + n + n + n + n + n + n + n;
+	    System.out.println("A divisão é: " + resultado);
+		System.out.println("O resto é: " + resto);
+		System.out.println ("A area é: " + area);
+		System.out.println("A multiplicação é: " + mult);
+
+	}
+
+}
